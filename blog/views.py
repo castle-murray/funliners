b00cb58b23f4e1ceb9cbbdae7d7757b544a9be2a
castle-murray <sean@castle-murray.com>
2022-11-19 00:33:31 -0500

    delete added, still need delete button

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
-                                  UpdateView
+                                  UpdateView,
+                                  DetailView,
+                                  DeleteView
                                   )
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -36,13 +38,30 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'use', 'problem', 'context', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
